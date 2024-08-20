c5f1ba17f298ae6ad64f05a8a1e82bd5afe8e7f5
bbenjamin99 <bbenjamin4253@gmail.com>
2024-08-20 15:54:29 -0300

    Agregando gifs..

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-
-
+import {Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn:'root'})
 
  export class GifsService{
+
     //Props
+    public gifList: Gif[] = [];
     private _tagHistory : string[] = [];
     private apiKey :      string = 'QcDM9Cf3ClZ6uxuD10HfDPF40zftD2Ct';
     private serviceUrl :  string = 'https://api.giphy.com/v1/gifs';
@@ -29,11 +30,14 @@ import { Injectable } from "@angular/core";
         .set('limit', 10)
         .set('q', tag)
 
-        this.http.get(`${this.serviceUrl}/search`, {params})
-        .subscribe(resp => {
-            console.log(resp);
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+        .subscribe(  resp  => {
+
+            this.gifList = resp.data;
+            console.log(this.gifList);
             
         })
+        
     }
 
  }
